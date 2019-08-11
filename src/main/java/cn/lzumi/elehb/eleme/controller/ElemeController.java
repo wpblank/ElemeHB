c1b6118ec4835c96f921a17b3ee7ea040e406a0d
wpblank <pyrdesu@outlook.com>
2019-08-11 13:27:52 +0800

    饿了么领取返回ret_code=12 无法解决

--- src/main/java/cn/lzumi/elehb/eleme/controller/ElemeController.java
+++ src/main/java/cn/lzumi/elehb/eleme/controller/ElemeController.java
@@ -35,6 +35,9 @@ public class ElemeController {
         return jsonObject.get("lucky_number");
     }
 
+    /**
+     *  不明白为什么返回 {"promotion_items":[],"promotion_records":[],"ret_code":12}
+     */
     @GetMapping("/get_one/{sn}")
     @ApiOperation(value = "领取一次红包", tags = {"饿了么"})
     public Object getOneHb(@PathVariable(value = "sn") String sn) {
@@ -42,20 +45,20 @@ public class ElemeController {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Accept", "*/*");
-        //httpHeaders.add("User-Agent", "Mozilla/5.0 (Linux; Android 5.1; m1 metal Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043409 Safari/537.36 V1ANDSQ7.2.5744YYBD QQ/7.2.5.3305 NetType/WIFI WebP/0.3.0 Pixel/1080");
+        httpHeaders.add("User-Agent", "Mozilla/5.0 (Linux; Android 5.1; m1 metal Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043409 Safari/537.36 V1ANDSQ7.2.5744YYBD QQ/7.2.5.3305 NetType/WIFI WebP/0.3.0 Pixel/1080");
         httpHeaders.add("Accept-Encoding", "gzip, deflate, br");
         httpHeaders.add("Accept-Language", "zh-CN,zh-HK;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6,zh-TW;q=0.5");
-        httpHeaders.add("Connection", "Keep-Alive");
+        //httpHeaders.add("Connection", "Keep-Alive");
         httpHeaders.add("Content-Type", "application/json;charset=UTF-8");
         httpHeaders.add("Cookie", "SID=ZdKkACRNv6waReXjFue6z8PkFgUWAmOGDAmg");
-        httpHeaders.add("Cache-Control","no-cache");
-        httpHeaders.add("host","h5.ele.me");
+        httpHeaders.add("Cache-Control", "no-cache");
+        httpHeaders.add("Host", "h5.ele.me");
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        //requestBody.add("sign", "61dcd338f5e0f7eb520788b262cb4ea");
-        requestBody.add("group_sn", "2a495ad6ed2dec2d.2");
+        requestBody.add("sign", "61dcd338f5e0f7eb520788b262cb4ea");
+        requestBody.add("group_sn", "2a4935b948076082.2");
         HttpEntity<MultiValueMap> requestEntity = new HttpEntity<>(requestBody, httpHeaders);
         ResponseEntity<String> responseEntity = restTemplate
-                .exchange(getElemeUrl + "B92BCA958221F6FFEE01FCBE8AC955D7",HttpMethod.POST, requestEntity, String.class);
+                .exchange(getElemeUrl + "B92BCA958221F6FFEE01FCBE8AC955D7", HttpMethod.POST, requestEntity, String.class);
         JSONObject jsonObject = JSON.parseObject(responseEntity.getBody());
         System.out.println(responseEntity);
         System.out.println(responseEntity.getBody());
