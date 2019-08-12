523221a624a08d58c7e061b6453c09aac61e48ca
izumi <pyrdesu@outlook.com>
2019-08-12 15:22:01 +0800

    v1.1 实现post领取功能 不要设置Content-Type等 requestHeaders.add("Content-Type", "application/json;charset=UTF-8");

--- src/main/java/cn/lzumi/elehb/eleme/controller/ElemeController.java
+++ src/main/java/cn/lzumi/elehb/eleme/controller/ElemeController.java
@@ -40,35 +40,28 @@ public class ElemeController {
     }
 
     /**
-     * 不明白为什么返回 {"promotion_items":[],"promotion_records":[],"ret_code":12}
+     *
      */
     @GetMapping("/get_one/{sn}")
     @ApiOperation(value = "领取一次红包", tags = {"饿了么"})
     public Object getOneHb(@PathVariable(value = "sn") String sn) {
         String openId = "B92BCA958221F6FFEE01FCBE8AC955D7";
-        String getElemeUrl = "https://h5.ele.me/restapi/marketing/v2/promotion/weixin/";
+        String getElemeUrl = "https://h5.ele.me/restapi/marketing/v2/promotion/weixin/B92BCA958221F6FFEE01FCBE8AC955D7";
         HttpHeaders requestHeaders = new HttpHeaders();
-        requestHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
         requestHeaders.add("Accept", "*/*");
         requestHeaders.add("User-Agent", "Mozilla/5.0 (Linux; Android 5.1; m1 metal Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043409 Safari/537.36 V1ANDSQ7.2.5744YYBD QQ/7.2.5.3305 NetType/WIFI WebP/0.3.0 Pixel/1080");
-        requestHeaders.add("Accept-Encoding", "gzip, deflate, br");
         requestHeaders.add("Accept-Language", "zh-CN,zh-HK;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6,zh-TW;q=0.5");
         requestHeaders.add("Connection", "Keep-Alive");
-        requestHeaders.add("Content-Type", "text/plain;charset=UTF-8");
         requestHeaders.add("Cookie", "SID=ZdKkACRNv6waReXjFue6z8PkFgUWAmOGDAmg");
-        requestHeaders.add("Cache-Control", "no-cache");
-        requestHeaders.add("Host", "h5.ele.me");
-        requestHeaders.add("content-length", "65");
-        requestHeaders.add("X-Shard", "eosid=3047066502041562000;loc=116.3440686,40.0046365");
 
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("sign", "61dcd338f5e0f7eb520788b262cb4ea");
+        requestBody.add("sign", "61dcd338f5e0f7eb520788b262cb4eaa");
         requestBody.add("group_sn", "2a4935b948076082.2");
 
         HttpEntity<MultiValueMap> requestEntity = new HttpEntity<>(requestBody, requestHeaders);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate
-                .exchange(getElemeUrl + openId, HttpMethod.POST, requestEntity, String.class);
+                .exchange(getElemeUrl, HttpMethod.POST, requestEntity, String.class);
         JSONObject jsonObject = JSON.parseObject(responseEntity.getBody());
         System.out.println(responseEntity);
         System.out.println(responseEntity.getHeaders());
