3ba008869a86c192e0b26ec02b67f57568a13b95
wpblank <pyrdesu@outlook.com>
2019-09-02 23:17:33 +0800

    饿了么星选定时领取

--- src/main/java/cn/lzumi/elehb/scheduled/ScheduledTasks.java
+++ src/main/java/cn/lzumi/elehb/scheduled/ScheduledTasks.java
@@ -36,9 +36,34 @@ public class ScheduledTasks {
     private ElemeStarCookie elemeStarCookie = new ElemeStarCookie();
 
     @Scheduled(fixedDelay = 60000)
-    public void getElemeStarHb(){
-        elemeStarCookie.cookie = "WMID=7d618e17882db95b74c92e3add15b306; whid=cjFHN2p6UFNFZkZUcGtldGxyS2I0UnZCSllpTzZRTWFBdVh4RGNJeU5MOW93OFYyM2RXMFVDYnpCcGVuWXhRVGxmTURCcFZtUmFRbnBTV2s1MGVuRkJSblJZVFE9PQ%3D%3D; WMST=1567421042";
-        String result = elemeStarUtils.getOne()
+    public void getElemeStarHb() {
+        if (once != 3) {
+            elemeStarCookie.cookie = "WMID=7d618e17882db95b74c92e3add15b306; whid=cjFHN2p6UFNFZkZUcGtldGxyS2I0UnZCSllpTzZRTWFBdVh4RGNJeU5MOW93OFYyM2RXMFVDYnpCcGVuWXhRVGxmTURCcFZtUmFRbnBTV2s1MGVuRkJSblJZVFE9PQ%3D%3D; WMST=1567421042";
+            String result = elemeStarUtils.getOne("988078338", "1ae6facf43540c507a915eca6092e9c2", elemeStarCookie);
+            int luckyNum = elemeStarUtils.getMaxNumberFromHtml(result);
+            int nowNum = elemeStarUtils.getNowNumberFromHtml(result);
+            if (luckyNum - nowNum == 1) {
+                elemeStarCookie.cookie = "";
+                result = elemeStarUtils.getOne("988078338", "1ae6facf43540c507a915eca6092e9c2", elemeStarCookie);
+                logger.info(result);
+                once++;
+            } else if (luckyNum <= nowNum) {
+                logger.info("没领到");
+            }
+
+            elemeStarCookie.cookie = "WMID=7d618e17882db95b74c92e3add15b306; whid=cjFHN2p6UFNFZkZUcGtldGxyS2I0UnZCSllpTzZRTWFBdVh4RGNJeU5MOW93OFYyM2RXMFVDYnpCcGVuWXhRVGxmTURCcFZtUmFRbnBTV2s1MGVuRkJSblJZVFE9PQ%3D%3D; WMST=1567421042";
+            result = elemeStarUtils.getOne("988243043", "00488e9e61f20694a9fb28164182a847", elemeStarCookie);
+            luckyNum = elemeStarUtils.getMaxNumberFromHtml(result);
+            nowNum = elemeStarUtils.getNowNumberFromHtml(result);
+            if (luckyNum - nowNum == 1) {
+                elemeStarCookie.cookie = "";
+                result = elemeStarUtils.getOne("988243043", "00488e9e61f20694a9fb28164182a847", elemeStarCookie);
+                logger.info(result);
+                once++;
+            } else if (luckyNum <= nowNum) {
+                logger.info("没领到");
+            }
+        }
     }
 //
 //    @Scheduled(fixedDelay = 86400000)
